99995b6c656f8ce3d35874142a018061cd7fc44c
Blastz13 <sinicinr2003@mail.ru>
2022-08-23 16:35:43 +0300

    Added 3 behavior validation

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
   <form v-on:submit.prevent>
+    <p>$model</p>
     <input
         id="name"
-        v-model.trim="name"
-        @input="v$.name.$touch()"
+        v-model="v$.name.$model"
     />
     <p>{{ v$.name.$errors }}</p>
 
+    <p>@blur</p>
     <input
         id="desc"
         v-model.trim="desc"
-        @input="v$.desc.$touch()"
+        @blur="v$.desc.$touch()"
+    />
+
+    <p>{{ v$.desc.$errors }}</p>
+
+    <p>onInput</p>
+    <input
+        id="desc2"
+        v-model.trim="desc2"
+        @input="v$.desc2.$touch()"
     />
     <input type="submit" @click="sub">
-    <p>{{ v$.desc.$errors }}</p>
+    <p>{{ v$.desc2.$errors }}</p>
 
 
 <!--    <div v-if="$v.name.$dirty">-->
@@ -35,7 +45,8 @@ export default {
   data () {
     return {
       name: '',
-      desc: ''
+      desc: '',
+      desc2: ''
     }
   },
   methods: {
@@ -43,17 +54,18 @@ export default {
       this.v$.$validate()
       console.log(this.v$.errors);
 
-      if(this.v$.error){
-        return false;
-      } else {
-        ;
-      }
+      // if(this.v$.error){
+      //   return false;
+      // } else {
+      //   ;
+      // }
     }
   },
   validations () {
     return {
       name: { required, minLength: minLength(6) }, // Matches this.firstNam
       desc: { required, minLength: minLength(10) }, // Matches this.firstNam
+      desc2: { required, minLength: minLength(10) }, // Matches this.firstNam
     }
   }
 }
